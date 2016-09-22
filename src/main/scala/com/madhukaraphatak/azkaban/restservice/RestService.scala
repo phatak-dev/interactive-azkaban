de1ea0823a07814d6c7af8754a103522fd5f73f5
madhu <phatak.dev@gmail.com>
2016-09-22 08:22:54 +0530

    reformated

--- src/main/scala/com/madhukaraphatak/azkaban/restservice/RestService.scala
+++ src/main/scala/com/madhukaraphatak/azkaban/restservice/RestService.scala
@@ -15,54 +15,56 @@ import spray.json.{DefaultJsonProtocol, JsArray, pimpAny}
 import spray.json.DefaultJsonProtocol._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql._
-/**
- * Created by madhu on 8/11/15.
- */
-trait RestService {
-	implicit val system:ActorSystem
-	implicit val materializer:ActorMaterializer
-	implicit val sparkSession:SparkSession
-	val datasetMap = new ConcurrentHashMap[String,Dataset[Row]]()
 
-	import ServiceJsonProtoocol._
-	val route =
-		pathSingleSlash {
-			get {
-				complete {
-					"welcome to rest service"
-				}
-			}
-		} ~
-	path("load") {
-		post {
-			entity(as[LoadRequest]) {
-				loadRequest => complete {
-					val id = ""+System.nanoTime()
-					val dataset = sparkSession.read.format("csv")
-						.option("header","true")
-						.load(loadRequest.path)
-						datasetMap.put(id,dataset)
-						id
-				}
-			}
-		}
-	} ~
-	path("view" / """[\w[0-9]-_]+""".r) { id =>
-		get{
-			complete {
-				val dataset = datasetMap.get(id)
-				dataset.take(10).map(row => row.toString())
-			}
-		}
-	} ~
-  path("schedule" ) {
-		get{
-			complete {
-        val scheduler = new Scheduler()
-        scheduler.schedule(system,materializer)
-        "scheduled"
-			}
-		}
-	}
+/**
+  * Created by madhu on 8/11/15.
+  */
+trait RestService {
+  implicit val system: ActorSystem
+  implicit val materializer: ActorMaterializer
+  implicit val sparkSession: SparkSession
+  val datasetMap = new ConcurrentHashMap[String, Dataset[Row]]()
+
+  import ServiceJsonProtoocol._
+
+  val route =
+    pathSingleSlash {
+      get {
+        complete {
+          "welcome to rest service"
+        }
+      }
+    } ~
+      path("load") {
+        post {
+          entity(as[LoadRequest]) {
+            loadRequest => complete {
+              val id = "" + System.nanoTime()
+              val dataset = sparkSession.read.format("csv")
+                .option("header", "true")
+                .load(loadRequest.path)
+              datasetMap.put(id, dataset)
+              id
+            }
+          }
+        }
+      } ~
+      path("view" / """[\w[0-9]-_]+""".r) { id =>
+        get {
+          complete {
+            val dataset = datasetMap.get(id)
+            dataset.take(10).map(row => row.toString())
+          }
+        }
+      } ~
+      path("schedule") {
+        get {
+          complete {
+            val scheduler = new Scheduler()
+            scheduler.schedule(system, materializer)
+            "scheduled"
+          }
+        }
+      }
 
 }
